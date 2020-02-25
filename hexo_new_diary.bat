6f81d65a206af2d3c3f91db5ec73fa6a3daaa8b1
oscarcx123 <xuc15@pitt.edu>
2020-02-24 23:17:52 -0500

    Site updated: 2020-02-24 23:17:52

--- hexo_new_diary.bat
+++ hexo_new_diary.bat
@@ -4,11 +4,9 @@ SET today="%date:~0,4%%date:~5,2%%date:~8,2%"
 SET diaryFile="source/_posts/%today%.md"
 echo try to create %diaryFile%
 if exist %diaryFile% (
-    echo %diaryFile% exist!
+    echo %diaryFile% exist!&&echo open %diaryFile% with VSCode!&&code "%diaryFile%"
 ) else (
     echo %diaryFile% not exist!
     echo create %diaryFile%!
-    hexo new diary "%today%"
+    hexo new diary "%today%"&&echo open %diaryFile% with VSCode!&&code "%diaryFile%"
 )
-echo open %diaryFile% with VSCode!
-code "%diaryFile%"
--- hexo_one_click2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set /p confirm=Really want to backup and deploy? (y/n):
-if "%confirm%"=="y" (
-    echo starting...
-    call hexo_clean.bat
-    call hexo_backup.bat
-    call hexo_deploy.bat
-    echo done!
-)
