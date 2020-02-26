6db917e1eb659d0ec16b046f56b4fa9f2d59ce37
oscarcx123 <xuc15@pitt.edu>
2020-02-26 02:21:15 -0500

    Site updated: 2020-02-26 02:21:15

--- themes/next/source/aplayer/music.js
+++ themes/next/source/aplayer/music.js
@@ -6,32 +6,32 @@ const ap = new APlayer({
       {
         name: "宿星のガールフレンド３",
         artist: 'mirai',
-        url: 'https://raw.githubusercontent.com/oscarcx123/hexo_music/master/song/%E5%AE%BF%E6%98%9F%E3%81%AE%E3%82%AC%E3%83%BC%E3%83%AB%E3%83%95%E3%83%AC%E3%83%B3%E3%83%89%EF%BC%93.mp3',
-        cover: 'https://raw.githubusercontent.com/oscarcx123/hexo_music/master/cover/%E5%AE%BF%E6%98%9F%E3%81%AE%E3%82%AC%E3%83%BC%E3%83%AB%E3%83%95%E3%83%AC%E3%83%B3%E3%83%89%EF%BC%93.jpg',
+        url: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/song/%E5%AE%BF%E6%98%9F%E3%81%AE%E3%82%AC%E3%83%BC%E3%83%AB%E3%83%95%E3%83%AC%E3%83%B3%E3%83%89%EF%BC%93.mp3',
+        cover: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/cover/%E5%AE%BF%E6%98%9F%E3%81%AE%E3%82%AC%E3%83%BC%E3%83%AB%E3%83%95%E3%83%AC%E3%83%B3%E3%83%89%EF%BC%93.jpg',
       },
       {
         name: "宿星のガールフレンド",
         artist: 'mirai',
-        url: 'https://github.com/oscarcx123/hexo_music/raw/master/song/%E5%AE%BF%E6%98%9F%E3%81%AE%E3%82%AC%E3%83%BC%E3%83%AB%E3%83%95%E3%83%AC%E3%83%B3%E3%83%89.mp3',
-        cover: 'https://github.com/oscarcx123/hexo_music/raw/master/cover/%E5%AE%BF%E6%98%9F%E3%81%AE%E3%82%AC%E3%83%BC%E3%83%AB%E3%83%95%E3%83%AC%E3%83%B3%E3%83%89.jpg',
+        url: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/song/%E5%AE%BF%E6%98%9F%E3%81%AE%E3%82%AC%E3%83%BC%E3%83%AB%E3%83%95%E3%83%AC%E3%83%B3%E3%83%89.mp3',
+        cover: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/cover/%E5%AE%BF%E6%98%9F%E3%81%AE%E3%82%AC%E3%83%BC%E3%83%AB%E3%83%95%E3%83%AC%E3%83%B3%E3%83%89.jpg',
       },
       {
         name: "夏恋花火",
         artist: '40mP',
-        url: 'https://github.com/oscarcx123/hexo_music/raw/master/song/%E5%A4%8F%E6%81%8B%E8%8A%B1%E7%81%AB.mp3',
-        cover: 'https://github.com/oscarcx123/hexo_music/raw/master/cover/%E5%A4%8F%E6%81%8B%E8%8A%B1%E7%81%AB.jpg',
+        url: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/song/%E5%A4%8F%E6%81%8B%E8%8A%B1%E7%81%AB.mp3',
+        cover: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/cover/%E5%A4%8F%E6%81%8B%E8%8A%B1%E7%81%AB.jpg',
       },
       {
         name: "ETERNAL DRAIN",
         artist: 'Colorful Sounds Port',
-        url: 'https://github.com/oscarcx123/hexo_music/raw/master/song/ETERNAL%20DRAIN.mp3',
-        cover: 'https://github.com/oscarcx123/hexo_music/raw/master/cover/ETERNAL%20DRAIN.jpg',
+        url: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/song/ETERNAL%20DRAIN.mp3',
+        cover: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/cover/ETERNAL%20DRAIN.jpg',
       },
       {
         name: "Schubert Fantasy in F minor D940",
         artist: 'Schubert',
-        url: 'https://github.com/oscarcx123/hexo_music/raw/master/song/Schubert%20Fantasy%20in%20F%20minor%20D940.mp3',
-        cover: 'https://github.com/oscarcx123/hexo_music/raw/master/cover/Schubert%20Fantasy%20in%20F%20minor%20D940.jpg',
+        url: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/song/Schubert%20Fantasy%20in%20F%20minor%20D940.mp3',
+        cover: 'https://raw.githubusercontent.com/oscarcx123/hexo_resource/master/music/cover/Schubert%20Fantasy%20in%20F%20minor%20D940.jpg',
       }
     ]
 });
